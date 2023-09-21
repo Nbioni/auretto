63f39ea1f9aef82d46ac5c5f327ce5d77673797f
Nbioni <43020119+Nbioni@users.noreply.github.com>
2023-09-20 22:31:04 -0300

    mutate video

--- src/components/VideoBackground.tsx
+++ src/components/VideoBackground.tsx
@@ -5,7 +5,7 @@ export function VideoBackground() {
     <div className=" bg-auretto-dark-blue w-screen h-screen">
       <div className="flex items-start 
            w-screen h-screen overflow-hidden opacity-70 contrast-[0.85] sepia-[0.07]">
-        <video autoPlay loop className="absolute top-0 w-screen h-screen object-cover" src="https://github.com/Nbioni/auretto/raw/main/public/maringa-video.mp4" />
+        <video autoPlay loop muted className="absolute top-0 w-screen h-screen object-cover" src="https://github.com/Nbioni/auretto/raw/main/public/maringa-video.mp4" />
       </div>
     </div>
   );
